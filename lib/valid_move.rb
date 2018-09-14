64e04aeed4f10bb43e883c3e844833c5c3f21cc5
Nick Williams <nwilliams7539@student.whatcom.edu>
2018-09-14 20:13:01 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   board = [" "," "," "," "," "," "," "," "," ",]
-  if board[index].between?(0, 8)
+  if board[index].between?(board[0], board[8])
     return true
 end
 end
